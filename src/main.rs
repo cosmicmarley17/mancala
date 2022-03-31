c1eebff9e0ae517a0c65f00f980d6f0384e85274
Marley Sullivan <cosmicmarley17@protonmail.com>
2022-03-30 23:01:28 -0400

    Start laying basic framework in main.rs     * Add board arrays     * Add function for playing turns     * Add enum for whose turn it is     * Add enum for which pit is being moved

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,43 @@
 fn main() {
-    println!("Hello world!");
+    let mut winner;
+    let mut p1_board = [4,4,4,4,4,4,0];
+    let mut p2_board = [4,4,4,4,4,4,0];
+    loop {
+        //TODO consider combining p1 and p2 boards into a unified board struct
+        //  - would the stores become their own data structures?
+        play_turn(Turn::P1, &mut p1_board, &mut p2_board);
+        play_turn(Turn::P2, &mut p1_board, &mut p2_board);
+    }
+}
+
+// player whose turn it is
+enum Turn {
+    P1,
+    P2,
+}
+
+// which pit the player has selected to move
+enum Move {
+    A,
+    B,
+    C,
+    D,
+    E,
+    F,
+}
+
+fn play_turn(player: Turn, p1_board: &mut [u32;7], p2_board: &mut [u32;7]) {
+    draw_board();   // paint the TUI
+
+    // get player input for move here
+    let mut move_input: Move;
+
+    if check_gameover() {
+        //distribute leftover points, count store totals, and declare winner
+    }
+}
+
+// paints the board in TUI
+fn draw_board(p1_board: &[u32;7], p2_board: &[u32;7]) {
+
 }
