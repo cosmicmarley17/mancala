86f727531886e564702331d9f58480db7a91e778
Marley Sullivan <cosmicmarley17@protonmail.com>
2022-04-13 17:23:11 -0400

    Work on draw_board() function * Work in progress, printing opposite player's side and middle with   store values

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,12 @@
 fn main() {
     let mut winner = 0;
     let mut board = MancalaBoard::new();
-    loop {
-        play_turn(Turn::P1, &mut board);
-        play_turn(Turn::P2, &mut board);
-    }
+    // Uncomment loop when done debugging
+    // loop {
+    //     play_turn(Turn::P1, &mut board);
+    //     play_turn(Turn::P2, &mut board);
+    // }
+    play_turn(Turn::P1, &mut board);    //DEBUG
 }
 
 // player whose turn it is
@@ -55,10 +57,47 @@ fn play_turn(player: Turn, board: &mut MancalaBoard) {
 
 // paints the board in TUI
 fn draw_board(board: &MancalaBoard) {
+    // TODO add a function param for current player, so the opposite side is drawn accordingly
+    // rather than hardcoded from player 1's POV
+
     println!("TODO: write draw_board()");
+    println!("");
+    //print opposite player's side
+    // println!("[    ] ({}) ({}) ({}) ({}) ({}) ({}) [    ]", );
+    print!("[    ] ");
+    for i in board.p2_board {
+        let pit = i;
+        let pit = pad_number(pit);
+        print!("({}) ", pit)
+    }
+    print!("[    ] \n");
+
+    //print middle gap + stores
+    let store_left = pad_number(board.p2_store);
+    let store_right = pad_number(board.p1_store);
+    println!("[ {} ]                               [ {} ]", store_left, store_right);
+
+    //print current player's side
+    // println!("[    ] ({}) ({}) ({}) ({}) ({}) ({}) [    ]");
+
+    //print bottom (board labels)
+    // println!("         A    B    C    D    E    F   STORE");
+
+    // println!("{:?}", board.p1_board[2]);
+}
+
+// converts a number to a string and
+// adds a space to the beginning if the number is a single digit.
+fn pad_number(num: u32) -> String {
+    if num > 9 {
+        return num.to_string();
+    }
+    else {
+        return " ".to_string() + &num.to_string();
+    }
 }
 
 fn check_gameover() -> bool {
-    println!("TODO: write check_gameover()");
+    //TODO: write check_gameover()
     false
 }
