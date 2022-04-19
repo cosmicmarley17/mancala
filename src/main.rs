cf2c81e40b9e1817634821e87b053f42e14cde24
Marley Sullivan <cosmicmarley17@protonmail.com>
2022-04-18 23:08:53 -0400

    Improve visual debug instantiator for MancalaBoard * The new values make it easier to discern perspective

--- src/main.rs
+++ src/main.rs
@@ -50,7 +50,7 @@ impl MancalaBoard {
     pub fn new_visual_debug() -> Self {
         Self {
             p1_board: [1, 2, 3, 4, 5, 6],
-            p2_board: [1, 2, 3, 4, 5, 6],
+            p2_board: [7, 8, 9, 10, 11, 12],
             p1_store: 1,
             p2_store: 2,
         }
